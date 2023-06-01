001fb000e3602649be33bc5d501497a6ace4e9e5
jayr pongase <jayrpongase143@gmail.com>
2023-06-01 18:04:58 +0800

    update texts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,8 @@ const Hero = () => {
         <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4'>
             <h1 className='text-black font-bold text-4xl'>Find Your Special Trip</h1>
             <h2 className='text-4xl text-blue-700 py-4 italic'>With J Travel</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Donec et odio vitae lectus lobortis pellentesque. 
-            Ut ut velit mi. Nam quam eros, dictum quis malesuada ac, 
-            ultricies vel mi. Mauris finibus nisl magna, ut posuere 
-            justo vehicula non. Maecenas blandit blandit risus, at 
-            venenatis quam varius efficitur. Ut vitae dolor tortor. 
-            Aenean nunc nisl, commodo sed ornare eget, commodo et mauris. 
+            <p className='text-xl'> J Travel assist of your needs for your special trip. We book tickets 
+              according to your preferred destinations. Feel free to contact us. Happy to serve you!
             </p>
         </div>
       </div>
